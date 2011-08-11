4263396d4a4a5f63a24806423e0b94e39f237a6d
thekeeper <coder@ukuzoo.com>
2011-08-11 13:06:54 -0700

    forgot a couple of header files

---
+++ UkuleleDazz/TouchJson.h
@@ -0,0 +1,11 @@
+//
+//  TouchJson.h
+//  UkuleleDazz
+//
+//  Created by Terry Tucker on 3/29/11.
+//  Copyright 2011 Terry Tucker. All rights reserved.
+//
+
+#import "CJSONSerializer.h"
+#import "CJSONDeSerializer.h"
+#import "CJSONScanner.h"
